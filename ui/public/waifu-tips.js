af3b2d380f20e36fcff25d6bca0b931ef1617f61
brain.duan <brain.duan@analyticservice.net>
2022-01-29 18:26:13 +0800

    remove rasa ui

--- ui/public/waifu-tips.js
+++ ui/public/waifu-tips.js
@@ -61,36 +61,40 @@ function loadWidget(config) {
     }, 1000);
 
     (function registerEventListener() {
-        document.querySelector("#waifu-tool .fa-comment").addEventListener("click", showHitokoto);
-        document.querySelector("#waifu-tool .fa-paper-plane").addEventListener("click", () => {
-            if (window.Asteroids) {
-                if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
-                window.ASTEROIDSPLAYERS.push(new Asteroids());
-            } else {
-                const script = document.createElement("script");
-                script.src = "https://cdn.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
-                document.head.appendChild(script);
-            }
-        });
-        document.querySelector("#waifu-tool .fa-user-circle").addEventListener("click", loadOtherModel);
-        document.querySelector("#waifu-tool .fa-street-view").addEventListener("click", loadRandModel);
-        document.querySelector("#waifu-tool .fa-camera-retro").addEventListener("click", () => {
-            showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
-            Live2D.captureName = "photo.png";
-            Live2D.captureFrame = true;
-        });
-        document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
-            open("https://github.com/stevenjoezhang/live2d-widget");
-        });
-        document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
-            localStorage.setItem("waifu-display", Date.now());
-            showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
-            document.getElementById("waifu").style.bottom = "-500px";
-            setTimeout(() => {
-                document.getElementById("waifu").style.display = "none";
-                document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
-            }, 3000);
-        });
+        try {
+            document.querySelector("#waifu-tool .fa-comment").addEventListener("click", showHitokoto);
+            document.querySelector("#waifu-tool .fa-paper-plane").addEventListener("click", () => {
+                if (window.Asteroids) {
+                    if (!window.ASTEROIDSPLAYERS) window.ASTEROIDSPLAYERS = [];
+                    window.ASTEROIDSPLAYERS.push(new Asteroids());
+                } else {
+                    const script = document.createElement("script");
+                    script.src = "https://cdn.jsdelivr.net/gh/stevenjoezhang/asteroids/asteroids.js";
+                    document.head.appendChild(script);
+                }
+            });
+            document.querySelector("#waifu-tool .fa-user-circle").addEventListener("click", loadOtherModel);
+            document.querySelector("#waifu-tool .fa-street-view").addEventListener("click", loadRandModel);
+            document.querySelector("#waifu-tool .fa-camera-retro").addEventListener("click", () => {
+                showMessage("照好了嘛，是不是很可爱呢？", 6000, 9);
+                Live2D.captureName = "photo.png";
+                Live2D.captureFrame = true;
+            });
+            document.querySelector("#waifu-tool .fa-info-circle").addEventListener("click", () => {
+                open("https://github.com/stevenjoezhang/live2d-widget");
+            });
+            document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
+                localStorage.setItem("waifu-display", Date.now());
+                showMessage("愿你有一天能与重要的人重逢。", 2000, 11);
+                document.getElementById("waifu").style.bottom = "-500px";
+                setTimeout(() => {
+                    document.getElementById("waifu").style.display = "none";
+                    document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
+                }, 3000);
+            });
+        } catch (e) {
+
+        }
         const devtools = () => {};
         console.log("%c", devtools);
         devtools.toString = () => {
@@ -102,6 +106,7 @@ function loadWidget(config) {
         window.addEventListener("visibilitychange", () => {
             if (!document.hidden) showMessage("哇，你终于回来了～", 6000, 9);
         });
+
     })();
 
     (function welcomeMessage() {
